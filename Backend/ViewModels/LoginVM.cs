cae536670aeb8532a27b600fcdfa22263e02011e
Akmal Kamalov <khorezm0@yandex.ru>
2022-07-27 01:33:34 +0400

    Init project

---
+++ Backend/ViewModels/LoginVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.ViewModels
+{
+    public class LoginVM
+    {
+        //public string? Id { get; set;}
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ DAL/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL.Contracts
+{
+    public interface IRepository<T>
+    {
+        public Task<T> Create(T _object);
+        public void Delete(T _object);
+
+        public void Update(T _object);
+
+        public IEnumerable<T> GetAll();
+
+        public T GetById(int Id);
+
+    }
+}
